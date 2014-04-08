d7d3b95d9ec98e72917d5f5b1fcb935923399034
Pavel Forkert <fxposter@gmail.com>
2014-04-08 11:38:00 +0300

    Week #3: required part

--- task03/src/dsl/core.clj
+++ task03/src/dsl/core.clj
@@ -8,7 +8,7 @@
 
 (comment
   (defn one [] 1)
-  
+
   ;; Примеры вызова
   (with-datetime
     (if (> today tomorrow) (println "Time goes wrong"))
@@ -23,10 +23,81 @@
                (< d3 d4))
         (println "DSL works correctly")))))
 
+(def ^:private date-periods {'second java.util.Calendar/SECOND,
+                             'seconds java.util.Calendar/SECOND,
+                             'minute java.util.Calendar/MINUTE,
+                             'minutes java.util.Calendar/MINUTE,
+                             'hour java.util.Calendar/HOUR,
+                             'hours java.util.Calendar/HOUR,
+                             'day java.util.Calendar/DATE,
+                             'days java.util.Calendar/DATE,
+                             'week java.util.Calendar/WEEK_OF_YEAR,
+                             'weeks java.util.Calendar/WEEK_OF_YEAR,
+                             'month java.util.Calendar/MONTH,
+                             'months java.util.Calendar/MONTH,
+                             'year java.util.Calendar/YEAR,
+                             'years java.util.Calendar/YEAR})
+
+(def ^:private date-comparison-operators '#{> < >= <=})
+
+(def ^:private thread-local-calendar
+  (proxy [ThreadLocal] []
+    (initialValue []
+      (java.util.Calendar/getInstance))))
+
+(defn- calendar [] (.get thread-local-calendar))
+
+(defn modify-date [date field amount]
+  (->
+    (doto (calendar)
+      (.setTime date)
+      (.add field amount))
+    .getTime))
+
+(defn- is-date? [date]
+  (instance? java.util.Date date))
+
+(defn compare-dates-or-numbers [op date-or-number1 date-or-number2]
+  (if (and (is-date? date-or-number1)
+           (is-date? date-or-number2))
+    (op (.compareTo date-or-number1 date-or-number2) 0)
+    (op date-or-number1 date-or-number2)))
+
+(defn- matches-date-modification-dsl? [code]
+  (and (list? code)
+       (= (count code) 4)
+       (date-periods (last code))))
+
+(defn- transform-date-modification-dsl [[date op count period]]
+  `(modify-date ~date ~(date-periods period) (~op ~count)))
+
+(defn- apply-modification-transformations [code]
+  (if (matches-date-modification-dsl? code)
+    (transform-date-modification-dsl code)
+    code))
+
+(defn- matches-date-comparison-dsl? [code]
+  (and (list? code)
+       (= (count code) 3)
+       (date-comparison-operators (first code))))
+
+(defn- transform-date-comparison-dsl [[op date1 date2]]
+  `(compare-dates-or-numbers ~op ~date1 ~date2))
+
+(defn- apply-comparison-transformations [code]
+  (println code)
+  (if (matches-date-comparison-dsl? code)
+    (transform-date-comparison-dsl code)
+    code))
+
+(defn- transform-datetime-dsl [code]
+  (postwalk
+    (comp
+      apply-modification-transformations
+      apply-comparison-transformations)
+    code))
 
 ;; Режим Бога -- никаких подсказок.
 ;; Вы его сами выбрали ;-)
 (defmacro with-datetime [& code]
-  :ImplementMe!)
-
-
+  `(do ~@(transform-datetime-dsl code)))
